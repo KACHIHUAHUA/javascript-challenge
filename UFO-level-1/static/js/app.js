4caa72ef8c0fae911d38a02aa571393c94bce2e3
Valverde Martinez Carlos <cmchcvm@gcc.com>
2021-04-19 16:03:36 -0600

    version3

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,15 @@
-// from data.js
+// select the tabble data from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+function appendTable(data) {
+    d3.select("tbody").html("");
+    data.forEach((selection) => {
+        var tableRow = d3.select("tbody").append("tr");
+        Object.values(selection).forEach((value) => {
+            var tableData = tableRow.append("td");
+            tableData.text(value);
+        });
+    })
+}
+
+appendTable(tableData);
